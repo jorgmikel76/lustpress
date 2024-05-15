b2e8460243d72bc64d37c3703aea2fa301daefd5
jorgmikel76 <162457095+jorgmikel76@users.noreply.github.com>
2024-05-15 18:05:50 +0000

    Deploying to gh-pages from @ jorgmikel76/lustpress@6045712860d2bcb915fbd0ae2b9244dd2a1521c0 🚀

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Lustpress API Documentation",
+  "url": "https://lust.scathach.id",
+  "sampleUrl": "https://lust.scathach.id",
+  "name": "Lustpress",
+  "version": "1.6.3-alpha",
+  "description": "RESTful and experimental API for PornHub and other porn sites, which official is lack even isn't exist.",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2024-05-15T18:05:49.930Z",
+    "url": "https://apidocjs.com",
+    "version": "0.29.0"
+  }
+});
